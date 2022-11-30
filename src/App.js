eae0d865705e747cde64e8b617f2c8f0716ac98c
Roos de Jonge <rosemariedejonge@gmail.com>
2022-11-30 17:06:51 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather app</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="containter">
+        <h1>Test if this page is working</h1>
+        <footer>
+          This project was coded by Roos de Jonge and is{" "}
+          <a href="https://github.com/Roosdj/react-weather-app">
+            open-sourced at Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
